f0fb4e4d1ebd0ac4721ec9a93f6712403a69172b
nicolazgarcia <nicolazgarcia@gmail.com>
2015-09-04 02:58:37 -0500

    test only text
    me despido con verguenza de la posibilidad del audio

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,4 @@
-//http://stackoverflow.com/questions/30259395/audio-will-not-play-on-android-using-phonegap-but-works-fine-on-ios
-//http://simonmacdonald.blogspot.com/2011/05/using-media-class-in-phonegap.html
-//https://gist.github.com/alunny/2380994
 
-var playing = false;
-function effsound(src) {
-    if (device.platform == 'Android') {
-        src = '/android_asset/www/' + src;
-    }
-    var media = new Media(src, success, error_error);
-    media.play();
-    var playing = true;
-}
-function success() {
-    // ignore
-}
-function error_error(e) {
-    alert('great error');
-    alert(e.message);
-}
-
-function stopAudio() {
-  media.stop();
-  playing = false;   
-}
 	
 $('.cerrar').click(function(){
 	$('.mask').fadeOut();
@@ -30,33 +6,21 @@ $('.cerrar').click(function(){
 
 	$('.tipos li').removeClass('active');
 	$('.tarjeta').removeClass('show');
-
-    $('audio').each(function(){
-        this.pause(); // Stop playing
-       
-    });
-
 });
 
-$('.pieza h3').each(function() {
-	$(this).click(function() {
-  		$(this).next().addClass('show');
+$('.pieza').each(function() {
+    var pieza = $(this).find('.tarjeta');
+	$(this).find('h3').click(function() {
+  		$(pieza).addClass('show');
 	});
 	});
-	$('.volver').each(function() {
-    	$(this).click(function() {
-    		
-    		$('.tipos li').removeClass('active');
-
-      		$(this).parent().removeClass('show');
-
-            $('audio').each(function(){
-                this.pause(); // Stop playing
-                
-            });
-    	});
-	});
-	$.fn.clicktoggle = function(a, b) {
+	
+$('.volver').click(function() {
+    $('.tipos li').removeClass('active');
+    $('.tarjeta').removeClass('show');
+});
+	
+$.fn.clicktoggle = function(a, b) {
     return this.each(function() {
         var clicked = false;
         $(this).click(function() {
@@ -76,16 +40,8 @@ $('.pieza h3').each(function() {
 			$('.tipos li').removeClass('active');
 			$(this).addClass('active');
 		},function(b) {
-            $(this).topAudio();
 			$(this).removeClass('active');
 		});
-
-	/*.click(function() {
-	$('.tipos li').removeClass('active');
-		$(this).addClass('active');
-	}, function() {
-	$(this).removeClass('active');
-});*/
 
     $(document).click(function(event){
         if (!$(event.target).is('.search-box, .search-box *')) {
@@ -106,25 +62,9 @@ $('#map .zona').on({
        $('#map .zona').not(this).attr('fill-opacity',1.0);
     },
     click: function () {
-    	//$('#responsabilidad').attr('class', 'active');
-
     	var slice = $(this).attr('id');
     	$('article').hide();
   		$('.mask').fadeIn();
-    	$('.'+slice).fadeIn();
-
-    	/*
-    	$(this).attr('class', 'slice active');
-
-    	$(this).scrollPart();
-    	//agregar clase al padre
-    	$('#rg').removeClass(slice);
-    	$('#rg').addClass(slice );
-
-    	*/
-
-    	//$('.slices-content .part').hide();
-    	//$('.slices-content .part[data-slice=' + slice + ']').fadeIn(500);
-    	
+    	$('.'+slice).fadeIn();   	
 	}
 });
